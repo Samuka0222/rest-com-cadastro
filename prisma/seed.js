008ed1be92f318fb117473a04e02d5dd16085b8f
Samuka0222 <mmachado0222@gmail.com>
2024-04-13 16:48:15 -0300

    chore: add initial seed for Products Table

---
+++ prisma/seed.js
@@ -0,0 +1,45 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const seed = async () => {
+  try {
+    const products = [
+      {
+        name: "Notebook",
+        price: 1000,
+        description: "Notebook de 1000",
+      },
+      {
+        name: "Tablet",
+        price: 500,
+        description: "Tablet de 500",
+      },
+      {
+        name: "Mouse",
+        price: 200,
+        description: "Mouse de 200",
+      },
+      {
+        name: "Teclado",
+        price: 100,
+        description: "Teclado de 100",
+      },
+    ];
+
+    for (const product of products) {
+      await prisma.products.create({
+        data: {
+          name: product.name,
+          price: product.price,
+          description: product.description,
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error during seed:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+seed();
