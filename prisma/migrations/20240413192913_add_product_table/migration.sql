57507efa2dc2dd2ad550fc475c2d085f2a952e7a
Samuka0222 <mmachado0222@gmail.com>
2024-04-13 16:30:43 -0300

    chore: add initial prisma setup and product table

---
+++ prisma/migrations/20240413192913_add_product_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
